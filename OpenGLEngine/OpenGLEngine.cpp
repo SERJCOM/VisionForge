782eab01466fc50bbaa52a7acda6f710cd6303bd
SERJCOM <sergeylukin228@yandex.ru>
2022-04-14 02:44:13 +0300

    Добавьте файлы проекта.

---
+++ OpenGLEngine/OpenGLEngine.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <math.h>
+#include <SFML/Window/Mouse.hpp>
+#include <ctime>
+#include "PerlinNoise.hpp"
+#include <vector>
+#include "window.h"
+
+int main() {
+	Engine::Window window(100, 100);
+
+	return 0;
+}
---
+++ OpenGLEngine/window.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <GL/glew.h>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Texture.hpp>
+
+
+namespace Engine {
+	class Window {
+	public:
+		sf::Window window;
+		Window(int sizeX, int sizeY) {
+			window.create(sf::VideoMode(sizeX, sizeY), "OpenGL", sf::Style::Default, sf::ContextSettings(32));
+			window.setVerticalSyncEnabled(true);
+			window.setActive(true);
+	
+		}
+		void Display() {
+			window.display();
+			
+		}
+		
+	};
+}
