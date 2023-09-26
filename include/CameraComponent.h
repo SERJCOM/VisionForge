61d9ded8c2d3ed5132ec6a6e6ea31fec7bd6092a
sergei <sergeilukin228@yandex.ru>
2023-09-26 22:29:58 +0300

    improved camera rotation

--- include/CameraComponent.h
+++ include/CameraComponent.h
@@ -21,11 +21,6 @@ public:
 		Looking();
 		updateView();
 	}
-
-	glm::vec3 cameraPos = glm::vec3(-1.0f, 0.0f, 0.0f);
-	glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, 0.0f);
-	glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-	glm::mat4 view;
 	
 
 	CameraComponent() = default;
@@ -44,48 +39,9 @@ public:
 	}
 
 
-	virtual void Move() {
-		const float cameraSpeed = 0.5f;
-		
+	virtual void Move() ;
 
-		if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))	cameraPos += cameraSpeed * cameraFront;
-		if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))	cameraPos -= cameraSpeed * cameraFront;
-		if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))	cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-		if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))	cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-
-	}
-
-	virtual void Looking() {
-		
-		float xoffset = -(lastX - xpos);
-		float yoffset = -(ypos - lastY);
-
-		xoffset *= sensitivity_;
-		yoffset *= sensitivity_;
-
-		yaw += xoffset;	
-		pitch += yoffset;
-
-		if (pitch > 89.0f)
-			pitch = 89.0f;
-		if (pitch < -89.0f)
-			pitch = -89.0f;
-
-
-		glm::vec3 direction;
-		direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-		direction.y = sin(glm::radians(pitch));
-		direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-		cameraFront = glm::normalize(direction);
-
-
-		sf::Vector2i pos = sf::Mouse::getPosition(*window_);
-		std::swap(xpos, lastX);
-		std::swap(ypos, lastY);
-		xpos = pos.x;
-		ypos = pos.y;
-
-	}
+	virtual void Looking() ;
 
 	glm::mat4 updateView() {
 		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
@@ -111,10 +67,14 @@ public:
 	virtual ~CameraComponent() = default;
 
 protected:
-	float lastX = 400, lastY = 300;
 	float yaw = 0, pitch = 0;	
 	float xpos = 400, ypos = 300;	
+	float lastX = xpos, lastY = ypos;
 	float sensitivity_ = 0.05;
+	glm::vec3 cameraPos = glm::vec3(-1.0f, 0.0f, 0.0f);
+	glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::mat4 view;
 	sf::Window* window_;
 };
 
---
+++ src/CameraComponent.cpp
@@ -0,0 +1,51 @@
+#include "CameraComponent.h"
+#include <iostream>
+
+void lthm::CameraComponent::Move()
+{
+    const float cameraSpeed = 0.5f;
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))	cameraPos += cameraSpeed * cameraFront;
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))	cameraPos -= cameraSpeed * cameraFront;
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))	cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))	cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+
+}
+
+void lthm::CameraComponent::Looking()
+{
+    window_->setMouseCursorVisible(false);
+		
+    float xoffset = -(lastX - xpos);
+    float yoffset = -(ypos - lastY);
+
+
+    xoffset *= sensitivity_;
+    yoffset *= sensitivity_;
+
+    yaw += xoffset;	
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+
+
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.y = sin(glm::radians(pitch));
+    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(direction);
+
+    sf::Vector2i localPosition = sf::Mouse::getPosition(*window_);
+    xpos = localPosition.x;
+    ypos = localPosition.y;
+
+    sf::Vector2i Position;
+    Position.x = lastX;
+    Position.y = lastY;
+    sf::Mouse::setPosition(Position, *window_);
+
+}
